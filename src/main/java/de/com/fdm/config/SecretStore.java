47407d950dc8d19212e50a35fb136a2dd1b8605f
M4tthewDE <mattider2@gmail.com>
2022-05-19 22:21:06 +0200

    Turn SecretStore into record

--- src/main/java/de/com/fdm/config/SecretStore.java
+++ src/main/java/de/com/fdm/config/SecretStore.java
@@ -4,16 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SecretStore {
-    private final String secret;
-
-    public SecretStore(
-            @Value("${eventsub.clientSecret}") String secret
-    ) {
-        this.secret = secret;
-    }
-
-    public String getSecret() {
-        return secret;
-    }
-}
+public record SecretStore(
+        @Value("${eventsub.clientSecret}")
+        String secret
+) {}
